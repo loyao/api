1c89f53a9b5106acb03f18d1e1f6b79f021f0382
zhaoluyao <zhaoluyao@jangene.com>
2020-07-17 17:21:50 +0800

    init

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', ['namespace' => 'App\Http\Controllers\Api\v1'], function ($api) {
+    $api->group(['prefix' => 'auth'], function ($api) {
+        $api->post('register', 'AuthController@register');
+        $api->post('login', 'AuthController@login');
+        $api->get('/user/info', 'AuthController@index');
+    });
+});
